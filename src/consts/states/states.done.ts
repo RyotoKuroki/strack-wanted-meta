cfdbe06c8a8c8ac3c38556e26d5294f96e5bea43
RyotoKuroki <kurohoge@gmail.com>
2019-10-22 15:08:11 +0900

    const追加

---
+++ src/consts/states/states.done.ts
@@ -0,0 +1,9 @@
+/**
+ * ターゲット確保状態の定義
+ */
+export class DoneStates {
+    /** 未確保 */
+    static readonly YET: string = '';
+    /** 確保済み */
+    static readonly DONE: string = 'DONE';
+}
---
+++ src/consts/states/states.entity.enabled.ts
@@ -0,0 +1,9 @@
+/**
+ * エンティティ利用可否状態の定義
+ */
+export class EntityEnabledStates {
+    /** 利用可 */
+    static readonly ENABLED: string = 'ENABLED';
+    /** 利用不可 */
+    static readonly DISABLED: string = 'DISABLED';
+}
